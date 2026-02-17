5431969677e69a8a5eeb7faa0cf10c1e349bc8c7
Gabe Haynes <gabrielphaynes@gmail.com>
2026-02-16 22:10:27 -0500

    measurements of 800 are hard

--- projects/portrait/portrait.js
+++ projects/portrait/portrait.js
@@ -1,7 +1,14 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-const s = 800 / 600;
+// ---------------------
+// BACKGROUND
+// ---------------------
+ctx.fillStyle = "#3a6ea5"; // blue wall
+ctx.fillRect(0, 0, 600, 600);
 
-ctx.fillStyle = "#3a6ea5";
-ctx.fillRect(0, 0, 800, 800);
+// ---------------------
+// CHAIR
+// ---------------------
+ctx.fillStyle = "#111";
+ctx.fillRect(200, 300, 200, 250); // chair back
