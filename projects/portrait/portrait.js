b5a7ad1f38e85f370598026a23810a058e3fbf05
Gabe Haynes <gabrielphaynes@gmail.com>
2026-02-16 22:12:10 -0500

    chair and general head?

--- projects/portrait/portrait.js
+++ projects/portrait/portrait.js
@@ -10,5 +10,19 @@ ctx.fillRect(0, 0, 600, 600);
 // ---------------------
 // CHAIR
 // ---------------------
-ctx.fillStyle = "#111";
+ctx.fillStyle = "#111111";
 ctx.fillRect(200, 300, 200, 250); // chair back
+
+// ---------------------
+// NECK
+// ---------------------
+ctx.fillStyle = "#b97a57";
+ctx.fillRect(270, 330, 60, 80);
+
+// ---------------------
+// FACE
+// ---------------------
+ctx.beginPath();
+ctx.fillStyle = "##b97a57";
+ctx.arc(300, 250, 100, 0, Math.PI * 2);
+ctx.fill();
